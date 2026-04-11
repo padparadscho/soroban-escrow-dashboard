e36ac4077069e384730cbe598acf04f58c395db5
Padparadscho <contact@padparadscho.com>
2026-04-11 16:45:46 -0300

    feat(web): scaffold nextjs app structure

---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+// SPDX-FileCopyrightText: 2026 Padparadscho <contact@padparadscho.com>
+// SPDX-License-Identifier: GPL-3.0-only
+
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Soroban Escrow Dashboard",
+  description:
+    "An intuitive dashboard for monitoring a specified Soroban escrow smart contract, visualizing historical data, real-time events and transactions",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
