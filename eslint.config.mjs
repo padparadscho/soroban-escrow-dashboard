3dc9c43586830cbcce40e3bb37d89c0eabc3cfce
Padparadscho <contact@padparadscho.com>
2026-04-11 22:45:23 -0300

    chore(lint): update linting setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier";
 
-const eslintConfig = defineConfig([
+export default defineConfig([
   ...nextVitals,
   ...nextTs,
+  prettier,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
@@ -12,7 +14,7 @@ const eslintConfig = defineConfig([
     "out/**",
     "build/**",
     "next-env.d.ts",
+    // Custom ignores:
+    ".vscode/**",
   ]),
 ]);
-
-export default eslintConfig;
